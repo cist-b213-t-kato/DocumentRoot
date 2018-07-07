c15c896ee914233baf08f6e36b5450d1b0b37172
cist-b213-t-kato <cist.b213.t.kato@gmail.com>
2018-07-07 02:03:35 +0000

    お絵かきチャットを軽量化

--- node/app8.js
+++ node/app8.js
@@ -6,6 +6,7 @@ var io = require('socket.io')(app2);
 var fs = require('fs');
 var MongoClient = require("mongodb").MongoClient;
 var bodyParser = require("body-parser");
+var Canvas = require('canvas');
 
 
 
@@ -163,41 +164,62 @@ io.on('connection', function (socket) {
 			console.log('socket.emit()');
 		});
 
+		socket.on('drawListToServer', function(drawList) {
+
+			// console.log(drawList);
+			
+			//ここでDBに入れる
+			//dbに登録しようとした
+			imgCollection.find().sort({_id:-1}).limit(1).toArray((err, docs) => {
+					var canvas = new Canvas(2000, 2000);//document.getElementById('canvas');
+					Image = Canvas.Image;
+					var ctx = canvas.getContext('2d');
+
+					var img = new Image();
+					img.crossOrigin = "Anonymous";
+					img.src = docs[0].dat;
+					//console.log(docs[0].dat);
+					//console.log('img:'+img);
+					//console.log('img.src:'+img.src);
+
+					//img.onload = function() {
+					//console.log('image loaded');
+					ctx.drawImage(img, 0, 0);
+					ctx.beginPath();
+
+					for ( var i in drawList ) {
+						var drawData = drawList[i];
+						// console.log(drawData);
+						//マウス継続値によって場合分け、直線の moveTo（スタート地点）を決定
+						//継続値が初期値ではない場合は、前回のゴール位置を次のスタート位置とする
+						//lineTo（ゴール地点）の決定、現在のマウス位置をゴール地点とする
+						ctx.moveTo(drawData.startX, drawData.startY);
+						ctx.lineTo(drawData.endX, drawData.endY);
+
+						//直線の角を「丸」、サイズと色を決める
+						ctx.lineCap = "round";
+						ctx.lineWidth = drawData.size;
+						ctx.strokeStyle = drawData.color;
+						ctx.stroke();
+					}
+					
+					
+					var png = canvas.toDataURL('image/png');
+
+					// console.log('png: '+png);
+
+					imgCollection.updateOne({}, {$set:{dat:png}}, {upsert:true});
+					//};
+
+			})
+		});
+
 		socket.on('draw', function (drawData) {
 			// クライアントから受け取ったデータを出力する
 			// console.log('client: ' + data.startX);
 			// 全員に送信
 			socket.broadcast.emit('drawEmit', drawData);
 
-			//ここでDBに入れる
-			//dbに登録しようとした
-			collection.find().sort({_id:-1}).limit(1).toArray((err, docs) => {
-				var canvas = document.getElementById('canvas');
-				var ctx = canvas.getContext('2d');
-
-		    var img = new Image();
-		    img.crossOrigin = "Anonymous";
-		    img.src = docs[0].dat;
-				
-		    img.onload = function() {
-		     ctx.drawImage(img, 0, 0);
-		    }
-
-        ctx.beginPath();
-        //マウス継続値によって場合分け、直線の moveTo（スタート地点）を決定
-        //継続値が初期値ではない場合は、前回のゴール位置を次のスタート位置とする
-        //lineTo（ゴール地点）の決定、現在のマウス位置をゴール地点とする
-        // console.log(drawData.startX, drawData.startY);
-        ctx.moveTo(drawData.startX, drawData.startY);
-        ctx.lineTo(drawData.endX, drawData.endY);
-        //直線の角を「丸」、サイズと色を決める
-        ctx.lineCap = "round";
-        ctx.lineWidth = drawData.size;
-        ctx.strokeStyle = drawData.color;
-        ctx.stroke();
-
-				imgCollection.updateOne({}, {$set:req.body}, {upsert:true});
-			})
 
 		});
 
